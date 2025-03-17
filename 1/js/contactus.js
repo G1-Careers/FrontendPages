202d4c1398f77303a4c8481d59c4d7b36bd384eb
Sirisha-eng <sirisha@scaleorange.com>
2025-03-17 11:16:14 +0530

    Updated frontend pages

---
+++ 1/js/contactus.js
@@ -0,0 +1,12 @@
+
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-VNC7RV8M59');
+
+
+        function navigateToHome(){
+            window.location.href="https://www.hyno.co/"
+        }
+        navigateToHome()
